6f9f92dd3bc8fcbb2be9d085fcf4b2ea962d25f0
MohamTahaB <bayoumimt@gmail.com>
2023-11-04 01:33:53 +0100

    Add user and transaction info fetching

--- server/transactions/transaction.go
+++ server/transactions/transaction.go
@@ -2,12 +2,13 @@ package transactions
 
 import "time"
 
-// Define the transaction type
+// Define the transaction type.
 type Transaction struct {
 	Id          string
 	DisplayName string
 }
 
+// Define the transaction info type.
 type TransactionInfo struct {
 	Id     string
 	Amount float64
--- server/users/service.go
+++ server/users/service.go
@@ -4,11 +4,22 @@ import (
 	"fmt"
 )
 
-func (u *User) FetchUserInfo(userInfoMap map[string]*UserInfo) (*UserInfo, error) {
+// Fetches the user info from the user info map (temporary until a DB is made), knowing the id, outputs an error if not found.
+func (u *User) FetchUserInfoFromUser(userInfoMap map[string]*UserInfo) (*UserInfo, error) {
 	userInfoOutput, exists := userInfoMap[u.Id]
 	if exists {
 		return userInfoOutput, nil
 	} else {
 		return nil, fmt.Errorf("info of user of id : %s not found", u.Id)
+	}
+}
+
+// Fetches the user from the user map (temporary until a DB is made), knowing the id, outputs an error if not found.
+func (ui *UserInfo) FetchUserFromUserInfo(userMap map[string]*User) (*User, error) {
+	userOutput, exists := userMap[ui.Id]
+	if exists {
+		return userOutput, nil
+	} else {
+		return nil, fmt.Errorf("user of id : %s not found", ui.Id)
 	}
 }
