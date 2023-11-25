f2ba6b780d375165aac31b3b399c57610010f83d
MohamTahaB <bayoumimt@gmail.com>
2023-11-25 01:19:09 +0100

    basic layout

--- aspect/purse-app/src/App.tsx
+++ aspect/purse-app/src/App.tsx
@@ -1,13 +1,37 @@
-import Button from "react-bootstrap/Button";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { NavBar } from "./components/NavBar";
+import { Layout, theme } from "antd";
+import Sider from "antd/es/layout/Sider";
+import { Content, Footer } from "antd/es/layout/layout";
+import { useState } from "react";
 
 function App() {
+  const [sideCollapsed, setSideCollapsed] = useState<boolean>(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <>
-      <NavBar/>
-      <Button variant="primary">Primary</Button>{" "}
-    </>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        collapsible
+        collapsed={sideCollapsed}
+        onCollapse={(v) => setSideCollapsed(v)}
+      ></Sider>
+      <Layout>
+        <Content>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
   );
 }
 
